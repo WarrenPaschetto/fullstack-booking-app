799289d371c977e7d7e6ae252a5df225cf9b80d3
Warren Paschetto <wdpaschetto@gmail.com>
2025-05-04 12:00:26 -0400

    switched to Turso libsql Go driver

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,9 +4,12 @@ import (
 	"log"
 
 	"github.com/WarrenPaschetto/fullstack-booking-app/backend/internal/db"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
 	database, err := db.ConnectDB()
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
