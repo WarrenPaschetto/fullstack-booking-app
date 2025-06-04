a2bb371621a252a5d4ef0a90a0879255d2906bc7
Warren Paschetto <wdpaschetto@gmail.com>
2025-06-04 10:13:12 -0400

    rerun codecov

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -7,9 +7,9 @@ import (
 // CORS wraps any handler to add the proper headers.
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Allow your frontend origin
+		// Allow frontend origin
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		// Allow the methods your frontend might use
+		// Allow methods frontend might use
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		// Allow the headers (e.g. Content-Type and Authorization for JWT)
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
