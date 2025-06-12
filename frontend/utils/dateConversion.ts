e35d95a65b8a6c6a3a28248f0adeb564d5bc6506
Warren Paschetto <wdpaschetto@gmail.com>
2025-06-12 14:39:35 -0400

    added update booking functionality to admin

--- frontend/utils/dateConversion.ts
+++ frontend/utils/dateConversion.ts
@@ -1,16 +1,3 @@
-export function dateTimeToString(date: Date) {
-    const day = date.getDay;
-    const month = date.getMonth;
-    const year = date.getFullYear;
-    const hour = date.getHours;
-    const minutes = date.getMinutes;
-    return `${year}${month}${day}T${hour}:${minutes}Z`
-}
-
-export function stringToDate(date: string) {
-    const year = date.slice(0, 4)
-    const month = date.slice(4, 6)
-}
 
 export function formatAppointment(iso: string): string {
     const dt = new Date(iso);
